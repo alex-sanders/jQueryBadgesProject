19c701c1138549206a377f9cd1262c930252e956
alex-sanders <asanders.impaqt@gmail.com>
2017-05-11 09:24:32 -0400

    Step 5-6

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,28 @@
 $(function() {
 
   // your code will go here
+$.ajax({
+  url: "https://www.codeschool.com/users/Asanders11891.json",
+  dataType: 'jsonp',
+  success: function(response){
+      //reponse
+      addCourses(response.courses.completed);
+
+  }
+});
+
+
+
+function addCourses(courses) {
+  var $badges = $('#badges');
+  course.forEach(function(courses) {
+    $('<div />', {
+      'class': 'course'
+    }).appendTo($badges);
+
+  })
+}
+
+
 
 });
